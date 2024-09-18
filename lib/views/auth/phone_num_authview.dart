a0706e5af916e68469a851f28ae43b8e9a752346
ShouqAlqureshi <123020118+ShouqAlqureshi@users.noreply.github.com>
2024-09-18 04:56:28 +0300

    phone auth ui

--- lib/views/auth/phone_num_authview.dart
+++ lib/views/auth/phone_num_authview.dart
@@ -31,111 +31,133 @@ class _PhoneNumAuthViewState extends State<PhoneNumAuthView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 30),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              "Welcome to achiva ",
-              style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 40),
-            TextField(
-              autofocus: true,
-              controller: _phonenumber,
-              enableSuggestions: false,
-              autocorrect: false,
-              keyboardType: TextInputType.phone,
-              decoration: InputDecoration(
-                fillColor: Colors.grey.withOpacity(0.25),
-                filled: true,
-                hintText: "enter your Phone number here",
-                prefixIcon: const Icon(Icons.phone),
-                border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(30),
-                    borderSide: BorderSide.none),
-              ),
-            ),
-            const SizedBox(height: 20),
-            isloading
-                ? const CircularProgressIndicator()
-                : ElevatedButton(
-                    onPressed: () async {
-                      setState(() {
-                        isloading = true;
-                      });
-                      try {
-                        if (_phonenumber.text.isEmpty) {
-                          throw EmptyFieldException();
-                        }
-
-                        if (!RegExp(r'^\+[0-9]{10,14}$')
-                            .hasMatch(_phonenumber.text)) {
-                          throw InvalidPhoneNumberException(
-                              'Phone number must be between 10 and 12 digits long');
-                        }
-                        FirebaseAuth.instance.verifyPhoneNumber(
-                          phoneNumber: _phonenumber.text,
-                          verificationCompleted: (phoneAuthCredential) async {
-                            await FirebaseAuth.instance
-                                .signInWithCredential(phoneAuthCredential);
-                            final user = FirebaseAuth.instance.currentUser;
-                            if (user == null) {
-                              throw UserNotLoggedInAuthException();
+      appBar: AppBar(
+        title: Image.asset(
+          'lib/images/logo-with-name.png',
+          fit: BoxFit.contain,
+          height: 250,
+        ),
+        toolbarHeight: 150,
+        backgroundColor: null,
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 30),
+          child: Container(
+            decoration: BoxDecoration(
+                color: (Colors.deepPurple),
+                borderRadius: BorderRadius.circular(10)),
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  "Welcome to achiva ",
+                  style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.w500,
+                      color: Color.fromARGB(255, 240, 238, 249)),
+                ),
+                const SizedBox(height: 30),
+                TextField(
+                  autofocus: true,
+                  controller: _phonenumber,
+                  enableSuggestions: false,
+                  autocorrect: false,
+                  keyboardType: TextInputType.phone,
+                  decoration: InputDecoration(
+                    fillColor: Colors.white.withOpacity(0.25),
+                    filled: true,
+                    hintText: "enter your Phone number here",
+                    prefixIcon: const Icon(Icons.phone),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(30),
+                        borderSide: BorderSide.none),
+                  ),
+                ),
+                const SizedBox(height: 40),
+                isloading
+                    ? const CircularProgressIndicator()
+                    : ElevatedButton(
+                        onPressed: () async {
+                          setState(() {
+                            isloading = true;
+                          });
+                          try {
+                            if (_phonenumber.text.isEmpty) {
+                              throw EmptyFieldException();
                             }
-                          },
-                          verificationFailed:
-                              (FirebaseAuthException error) async {
+
+                            if (!RegExp(r'^\+[0-9]{10,14}$')
+                                .hasMatch(_phonenumber.text)) {
+                              throw InvalidPhoneNumberException(
+                                  'Phone number must be between 10 and 12 digits long');
+                            }
+                            FirebaseAuth.instance.verifyPhoneNumber(
+                              phoneNumber: _phonenumber.text,
+                              verificationCompleted:
+                                  (phoneAuthCredential) async {
+                                await FirebaseAuth.instance
+                                    .signInWithCredential(phoneAuthCredential);
+                                final user = FirebaseAuth.instance.currentUser;
+                                if (user == null) {
+                                  throw UserNotLoggedInAuthException();
+                                }
+                              },
+                              verificationFailed:
+                                  (FirebaseAuthException error) async {
+                                await showErrorDialog(
+                                  context,
+                                  'Check your Phone number formate:\n ${error.message}',
+                                );
+                              },
+                              codeSent:
+                                  (verificationId, forceResendingToken) async {
+                                setState(() {
+                                  _verificationId = verificationId;
+                                });
+                                Navigator.pushNamed(
+                                  context,
+                                  '/otp',
+                                  arguments: verificationId,
+                                );
+                              },
+                              timeout: const Duration(seconds: 60),
+                              codeAutoRetrievalTimeout: (verificationId) {
+                                devtool.log("auto retrireval timeout");
+                                setState(() {
+                                  _verificationId = verificationId;
+                                });
+                              },
+                            );
+                            setState(() {
+                              isloading = false;
+                            });
+                          } on EmptyFieldException {
                             await showErrorDialog(
                               context,
-                              'Oops..\nCheck your Phone number formate:\n ${error.message}',
+                              "Your phone number field is empty.\nPlease enter your phone number in the format:[+][country code][user number]",
                             );
-                          },
-                          codeSent:
-                              (verificationId, forceResendingToken) async {
-                            setState(() {
-                              _verificationId = verificationId;
-                            });
-                            Navigator.pushNamed(
+                          } on InvalidPhoneNumberException catch (e) {
+                            await showErrorDialog(
                               context,
-                              '/otp',
-                              arguments: verificationId,
+                              '${e.message}\nformat:[+][country code][user number]',
                             );
-                          },
-                          timeout: const Duration(seconds: 60),
-                          codeAutoRetrievalTimeout: (verificationId) {
-                            devtool.log("auto retrireval timeout");
+                          } catch (e) {
+                            await showErrorDialog(
+                                context, 'An unexpected error occurred: $e');
+                          } finally {
                             setState(() {
-                              _verificationId = verificationId;
+                              isloading = false;
                             });
-                          },
-                        );
-                        setState(() {
-                          isloading = false;
-                        });
-                      } on EmptyFieldException {
-                        await showErrorDialog(
-                          context,
-                          "Oops..\nYour phone number field is empty.\nPlease enter your phone number in the format:[+][country code][user number]",
-                        );
-                      } on InvalidPhoneNumberException catch (e) {
-                        await showErrorDialog(
-                          context,
-                          'Oops..\n${e.message}\nformat:[+][country code][user number]',
-                        );
-                      } catch (e) {
-                        await showErrorDialog(context,
-                            'Oops..\nAn unexpected error occurred: $e');
-                      } finally {
-                        setState(() {
-                          isloading = false;
-                        });
-                      }
-                    }, //on pressed
-                    child: const Text("Continue"),
-                  )
-          ],
+                          }
+                        }, //on pressed
+                        child: const Text("Continue"),
+                      )
+              ],
+            ),
+          ),
         ),
       ),
     );
--- lib/views/auth/sms_verification_authview.dart
+++ lib/views/auth/sms_verification_authview.dart
@@ -22,88 +22,104 @@ class _VerfyCodeViewState extends State<VerfyCodeView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Image.asset(
+          'lib/images/logo-with-name.png',
+          fit: BoxFit.contain,
+          height: 250,
+        ),
+        toolbarHeight: 150,
+        backgroundColor: null,
+        centerTitle: true,
+      ),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 30),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              "We have sent an OTP to your phone. Plz verify",
-              textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 18),
-            ),
-            const SizedBox(height: 40),
-            TextField(
-              controller: otpController,
-              keyboardType: TextInputType.phone,
-              decoration: InputDecoration(
-                  fillColor: Colors.grey.withOpacity(0.25),
-                  filled: true,
-                  hintText: "Enter OTP",
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(30),
-                      borderSide: BorderSide.none)),
-            ),
-            const SizedBox(height: 20),
-            isLoading
-                ? const CircularProgressIndicator()
-                : ElevatedButton(
-                    onPressed: () async {
-                      setState(() {
-                        isLoading = true;
-                      });
-                      try {
+        child: Container(
+          decoration: BoxDecoration(
+              color: (Colors.deepPurple),
+              borderRadius: BorderRadius.circular(10)),
+          padding: const EdgeInsets.all(8),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                "We have sent an OTP to your phone.\nPlease check your SMS massages",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 18, color: Colors.white),
+              ),
+              const SizedBox(height: 40),
+              TextField(
+                controller: otpController,
+                keyboardType: TextInputType.phone,
+                decoration: InputDecoration(
+                    fillColor: Colors.white.withOpacity(0.25),
+                    filled: true,
+                    hintText: "Enter OTP",
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(30),
+                        borderSide: BorderSide.none)),
+              ),
+              const SizedBox(height: 20),
+              isLoading
+                  ? const CircularProgressIndicator()
+                  : ElevatedButton(
+                      onPressed: () async {
+                        setState(() {
+                          isLoading = true;
+                        });
                         try {
-                          final verificationId = ModalRoute.of(context)!
-                              .settings
-                              .arguments as String;
-                          final cred = PhoneAuthProvider.credential(
-                              verificationId: verificationId,
-                              smsCode: otpController.text);
-                          final usercred = await FirebaseAuth.instance
-                              .signInWithCredential(cred);
-                          bool isNewUser_ =
-                              usercred.additionalUserInfo!.isNewUser;
-                          final userphonenumber = usercred.user?.phoneNumber;
-                          final datatosave = <String, dynamic>{
-                            "phoneNumber": userphonenumber
-                          };
-                          if (isNewUser_) {
-                            Navigator.pushNamed(context, "/newuser",
-                                arguments: datatosave);
-                          } else {
-                            Navigator.pushNamed(context, "/home");
+                          try {
+                            final verificationId = ModalRoute.of(context)!
+                                .settings
+                                .arguments as String;
+                            final cred = PhoneAuthProvider.credential(
+                                verificationId: verificationId,
+                                smsCode: otpController.text);
+                            final usercred = await FirebaseAuth.instance
+                                .signInWithCredential(cred);
+                            bool isNewUser_ =
+                                usercred.additionalUserInfo!.isNewUser;
+                            final userphonenumber = usercred.user?.phoneNumber;
+                            final datatosave = <String, dynamic>{
+                              "phoneNumber": userphonenumber
+                            };
+                            if (isNewUser_) {
+                              Navigator.pushNamed(context, "/newuser",
+                                  arguments: datatosave);
+                            } else {
+                              Navigator.pushNamed(context, "/home");
+                            }
+                          } on FirebaseAuthException catch (e) {
+                            log(e.toString());
+                            if (e.code == "invalid-verification-code") {
+                              throw InvalidVerificationCodeException(
+                                  "Please check and enter the correct verification code again.");
+                            } else if (otpController.text.isEmpty) {
+                              throw InvalidVerificationCodeException(
+                                  "the field is empty,\nplease write your sms code.");
+                            } else if (otpController.text.length < 6) {
+                              throw InvalidVerificationCodeException(
+                                  "the field is less than 6 digit,\nplease write the 6 digit sms code sent to you.");
+                            } else {
+                              throw GenricException();
+                            }
                           }
-                        } on FirebaseAuthException catch (e) {
-                          log(e.toString());
-                          if (e.code == "invalid-verification-code") {
-                            throw InvalidVerificationCodeException(
-                                "Oops..\nPlease check and enter the correct verification code again.");
-                          } else if (otpController.text.isEmpty) {
-                            throw InvalidVerificationCodeException(
-                                "Oops..\nthe field is empty,\nplease write your sms code.");
-                          } else if (otpController.text.length < 6) {
-                            throw InvalidVerificationCodeException(
-                                "Oops..\nthe field is less than 6 digit,\nplease write the 6 digit sms code sent to you.");
-                          } else {
-                            throw GenricException();
-                          }
+                        } on InvalidVerificationCodeException catch (e) {
+                          showErrorDialog(context, e.message);
+                        } on GenricException catch (e) {
+                          showErrorDialog(context, e.toString());
                         }
-                      } on InvalidVerificationCodeException catch (e) {
-                        showErrorDialog(context, e.message);
-                      } on GenricException catch (e) {
-                        showErrorDialog(context, e.toString());
-                      }
-                      setState(() {
-                        isLoading = false;
-                      });
-                    },
-                    child: const Text(
-                      "Verify",
-                      style:
-                          TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
-                    ))
-          ],
+                        setState(() {
+                          isLoading = false;
+                        });
+                      },
+                      child: const Text(
+                        "Verify",
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.w500),
+                      ))
+            ],
+          ),
         ),
       ),
     );
