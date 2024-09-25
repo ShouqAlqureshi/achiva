be55da56f65b7e23fb2f5a1e739ea7912e62e604
Miriam Almogren <miriamoffic@gmail.com>
2024-09-25 22:10:48 +0300

    updating depns

--- android/app/src/main/kotlin/com/example/achiva/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/achiva/MainActivity.kt
@@ -3,25 +3,25 @@ package com.example.achiva
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity()
-val integrityManager = IntegrityManagerFactory.create(applicationContext)
-val integrityTokenRequest = IntegrityTokenRequest.builder()
-    .setCloudProjectNumber(YOUR_PROJECT_NUMBER)
-    .build()
+// val integrityManager = IntegrityManagerFactory.create(applicationContext)
+// val integrityTokenRequest = IntegrityTokenRequest.builder()
+//     .setCloudProjectNumber(YOUR_PROJECT_NUMBER)
+//     .build()
 
-integrityManager.requestIntegrityToken(integrityTokenRequest)
-    .addOnSuccessListener { response ->
-        val token = response.token()
-        // Pass this token back to Flutter for further use.
-    }
-    .addOnFailureListener { exception ->
-        // Handle error.
-    }
-    val safetyNetClient = SafetyNet.getClient(this)
-safetyNetClient.verifyWithRecaptcha(SITE_KEY)
-    .addOnSuccessListener { response ->
-        val token = response.tokenResult
-        // Pass the token back to Flutter
-    }
-    .addOnFailureListener { exception ->
-        // Handle the error.
-    }
+// integrityManager.requestIntegrityToken(integrityTokenRequest)
+//     .addOnSuccessListener { response ->
+//         val token = response.token()
+//         // Pass this token back to Flutter for further use.
+//     }
+//     .addOnFailureListener { exception ->
+//         // Handle error.
+//     }
+//     val safetyNetClient = SafetyNet.getClient(this)
+// safetyNetClient.verifyWithRecaptcha(SITE_KEY)
+//     .addOnSuccessListener { response ->
+//         val token = response.tokenResult
+//         // Pass the token back to Flutter
+//     }
+//     .addOnFailureListener { exception ->
+//         // Handle the error.
+//     }
