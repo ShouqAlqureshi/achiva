33d63f5f65092f26611db1a948c45663fa562a25
ShouqAlqureshi <123020118+ShouqAlqureshi@users.noreply.github.com>
2024-10-03 00:41:20 +0300

    auth is fixed and runing

--- lib/main.dart
+++ lib/main.dart
@@ -9,11 +9,14 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'firebase_options.dart';
-
+import 'package:firebase_app_check/firebase_app_check.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
   options: DefaultFirebaseOptions.currentPlatform,
+  );
+   await FirebaseAppCheck.instance.activate(
+    androidProvider: AndroidProvider.playIntegrity,
   );
   runApp(BlocProvider(
     create: (context) => LayoutCubit(),
