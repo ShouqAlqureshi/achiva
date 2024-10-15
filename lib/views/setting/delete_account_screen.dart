ce576c8032734d5a3372b2148dadfe61bea3f7bc
ahad <439200907@student.ksu.edu.sa>
2024-10-15 08:56:19 +0300

    profile updates

--- lib/views/setting/delete_account_screen.dart
+++ lib/views/setting/delete_account_screen.dart
@@ -1,10 +1,11 @@
-import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'dart:developer';
 
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 // import 'package:achiva/core/Constants/constants.dart';
 // import 'package:achiva/core/constants/extensions.dart';
@@ -174,12 +175,31 @@ import 'package:firebase_auth/firebase_auth.dart';
 // }
 
 class DeleteAccountScreen extends StatefulWidget {
+  const DeleteAccountScreen({super.key});
   @override
-  _DeleteAccountScreenState createState() => _DeleteAccountScreenState();
+  State<DeleteAccountScreen> createState() => _DeleteAccountScreenState();
 }
 
 class _DeleteAccountScreenState extends State<DeleteAccountScreen> {
   bool _isLoading = false;
+  Future<void> _reauthenticateAndDelete() async {
+    try {
+      final providerData =
+          FirebaseAuth.instance.currentUser?.providerData.first;
+
+      if (AppleAuthProvider().providerId == providerData!.providerId) {
+        await FirebaseAuth.instance.currentUser!
+            .reauthenticateWithProvider(AppleAuthProvider());
+      } else if (GoogleAuthProvider().providerId == providerData.providerId) {
+        await FirebaseAuth.instance.currentUser!
+            .reauthenticateWithProvider(GoogleAuthProvider());
+      }
+
+      await FirebaseAuth.instance.currentUser?.delete();
+    } catch (e) {
+      log("$e");
+    }
+  }
 
   // Method to delete user from Firestore and FirebaseAuth
   Future<void> _deleteAccount() async {
@@ -196,38 +216,87 @@ class _DeleteAccountScreenState extends State<DeleteAccountScreen> {
       }
 
       // Get user's phone number (or use user.uid if using UIDs as document IDs)
-      String? userPhoneNumber = user.uid;
+      String? userUid = user.uid;
 
-      if (userPhoneNumber == null) {
-        throw Exception("Phone number not found for the logged-in user.");
-      }
-
+      //delete subcollection goals and goals[tasks]
+      Future<QuerySnapshot> goals = FirebaseFirestore.instance
+          .collection('Users')
+          .doc(userUid)
+          .collection("goals")
+          .get();
+      goals.then((value) async {
+        for (var goalsDoc in value.docs) {
+          DocumentReference goalsDocRef = FirebaseFirestore.instance
+              .collection('Users')
+              .doc(userUid)
+              .collection("goals")
+              .doc(goalsDoc.id);
+          Future<QuerySnapshot> tasks = FirebaseFirestore.instance
+              .collection('Users')
+              .doc(userUid)
+              .collection("goals")
+              .doc(goalsDoc.id)
+              .collection("tasks")
+              .get();
+          tasks.then(
+            (value) async {
+              for (var tasksDoc in value.docs) {
+                DocumentReference tasksDocRef = FirebaseFirestore.instance
+                    .collection('Users')
+                    .doc(userUid)
+                    .collection("goals")
+                    .doc(goalsDoc.id)
+                    .collection("tasks")
+                    .doc(tasksDoc.id);
+                await tasksDocRef.delete();
+              }
+            },
+          );
+          await goalsDocRef.delete();
+        }
+      });
       // Delete the user document from Firestore
       DocumentReference userDocRef = FirebaseFirestore.instance
           .collection('Users')
-          .doc(userPhoneNumber); // Use user.uid if using UIDs
+          .doc(userUid); // Use user.uid if using UIDs
 
       await userDocRef.delete();
 
       // Delete the user from FirebaseAuth
-      await user.delete();
+      try {
+        await user.delete();
+      } on FirebaseAuthException catch (e) {
+        log("$e");
+
+        if (e.code == "requires-recent-login") {
+          await _reauthenticateAndDelete();
+        }
+      }
 
       // Show success message and navigate to the login screen
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Account deleted successfully.')),
-      );
+      if (mounted) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Account deleted successfully.')),
+        );
+      }
 
       // Navigate to the login screen (or any other screen you prefer)
-      Navigator.pushReplacementNamed(context, '/phoneauth');
+      if (mounted) {
+        Navigator.pushReplacementNamed(context, '/phoneauth');
+      }
     } catch (e) {
       // Handle any errors
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Error deleting account: $e')),
-      );
+      if (mounted) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Error deleting account: $e')),
+        );
+      }
     } finally {
-      setState(() {
-        _isLoading = false;
-      });
+      if (mounted) {
+        setState(() {
+          _isLoading = false;
+        });
+      }
     }
   }
 
@@ -251,7 +320,8 @@ class _DeleteAccountScreenState extends State<DeleteAccountScreen> {
                   const SizedBox(height: 20),
                   ElevatedButton(
                     style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.red, // Set button color to red for warning
+                      backgroundColor:
+                          Colors.red, // Set button color to red for warning
                     ),
                     onPressed: _deleteAccount,
                     child: const Text(
