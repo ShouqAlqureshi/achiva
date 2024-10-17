18f1ccb3fd1ded9c6f657295bfe915da9da35e75
Riyam <88596299+RIYAM-M-S@users.noreply.github.com>
2024-10-17 09:28:30 +0300

    change

--- lib/views/SearchFriendsScreen.dart
+++ lib/views/SearchFriendsScreen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter/services.dart';
 
 class SearchFriendsScreen extends StatefulWidget {
   @override
