e9cc666894732e04a911634dba6dcac929d5d8d0
Amal Alharbi <amal.m.alharb@gmail.com>
2025-04-22 21:59:58 +0300

    CreateEditProfileScreen .dart

---
+++ EditProfileScreen .dart
@@ -0,0 +1,316 @@
+
+//EditProfileScreen  after Refactoring ******
+
+import 'dart:developer';
+import 'package:achiva/core/constants/extensions.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:image_picker/image_picker.dart';
+
+import '../../core/components/awesome_dialog_widget.dart';
+import '../../core/components/btn_widgets.dart';
+import '../../core/components/drop_down_button.dart';
+import '../../core/components/showSnackBar.dart';
+import '../../core/components/textField_widget.dart';
+import '../../core/constants/constants.dart';
+import '../../core/theme/app_colors.dart';
+import 'layout_controller/layout_cubit.dart';
+import 'layout_controller/layout_states.dart';
+
+class EditProfileScreen extends StatefulWidget {
+  final LayoutCubit layoutCubit;
+
+  const EditProfileScreen({super.key, required this.layoutCubit});
+
+  @override
+  State<EditProfileScreen> createState() => _EditProfileScreenState();
+}
+
+class _EditProfileScreenState extends State<EditProfileScreen> {
+  final TextEditingController _fnameController = TextEditingController();
+  final TextEditingController _lnameController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  GlobalKey<FormState> formState = GlobalKey<FormState>();
+  bool isFormSubmited = false;
+
+  List<String> emails = [];
+
+  void setUserDataToTextFields() {
+    widget.layoutCubit.userImage = null;
+    _fnameController.text = widget.layoutCubit.user!.fname;
+    _lnameController.text = widget.layoutCubit.user!.lname;
+    _emailController.text = widget.layoutCubit.user!.email;
+    widget.layoutCubit.chosenGender = widget.layoutCubit.user!.gender;
+  }
+
+  @override
+  void initState() {
+    setUserDataToTextFields();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _fnameController.dispose();
+    _lnameController.dispose();
+    _emailController.dispose();
+    widget.layoutCubit.chosenGender = null;
+    widget.layoutCubit.userImage = null;
+    super.dispose();
+  }
+
+  bool isValidEmail(String email) {
+    return RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$').hasMatch(email);
+  }
+
+  bool isFormValid(BuildContext context) {
+    if (_fnameController.text.isEmpty ||
+        _lnameController.text.isEmpty ||
+        _emailController.text.isEmpty ||
+        widget.layoutCubit.chosenGender == null) {
+      showSnackBarWidget(
+        message: "Please fill in all fields",
+        successOrNot: false,
+        context: context,
+      );
+      return false;
+    }
+    if (!isValidEmail(_emailController.text)) {
+      showSnackBarWidget(
+        message: "Please enter a valid email address",
+        successOrNot: false,
+        context: context,
+      );
+      return false;
+    }
+    return true;
+  }
+
+  Future<bool> checkIfEmailExists(String email) async {
+    try {
+      var response = await FirebaseFirestore.instance.collection("Users").get();
+      for (var email in response.docs) {
+        emails.add(email.data()["email"].toString().toLowerCase());
+      }
+      return emails.contains(email.toLowerCase());
+    } catch (e) {
+      print('Error: $e');
+      return false;
+    }
+  }
+
+  Future<void> updateUserData() async {
+    try {
+      if (!isFormValid(context)) return;
+
+      if (await checkIfEmailExists(_emailController.text) &&
+          _emailController.text != widget.layoutCubit.user!.email) {
+        showSnackBarWidget(
+          message: "Email already exists",
+          successOrNot: false,
+          context: context,
+        );
+        return;
+      }
+
+      await widget.layoutCubit.updateUserData(
+        fname: _fnameController.text.trim(),
+        lname: _lnameController.text.trim(),
+        gender: widget.layoutCubit.chosenGender!,
+        userID: widget.layoutCubit.user!.id,
+        email: _emailController.text.trim(),
+      );
+    } catch (e) {
+      showSnackBarWidget(
+        message: "Error updating profile: ${e.toString()}",
+        successOrNot: false,
+        context: context,
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: const Text("Edit Profile"),
+      ),
+      body: Stack(
+        children: [
+          Form(
+            key: formState,
+            child: ListView(
+              padding: AppConstants.kScaffoldPadding.copyWith(bottom: 24),
+              children: [
+                Center(
+                  child: Stack(
+                    alignment: Alignment.bottomRight,
+                    children: [
+                      BlocBuilder<LayoutCubit, LayoutStates>(
+                        buildWhen: (past, currentState) =>
+                            currentState is PickedUserImageSuccessfullyState,
+                        builder: (context, state) {
+                          if (widget.layoutCubit.userImage != null) {
+                            return CircleAvatar(
+                              radius: 64,
+                              backgroundImage:
+                                  FileImage(widget.layoutCubit.userImage!),
+                            );
+                          } else if (widget.layoutCubit.user!.photo != null) {
+                            return CircleAvatar(
+                              radius: 64,
+                              backgroundImage: NetworkImage(
+                                  widget.layoutCubit.user!.photo!),
+                            );
+                          } else {
+                            return CircleAvatar(
+                              radius: 64,
+                              backgroundColor: Colors.grey[200],
+                              child: const Icon(Icons.person,
+                                  size: 60, color: Colors.grey),
+                            );
+                          }
+                        },
+                      ),
+                      CircleAvatar(
+                        backgroundColor: AppColors.kLightGrey,
+                        child: InkWell(
+                          onTap: () {
+                            showImageSourceDialog(
+                              context: context,
+                              pickCameraImage: () => widget.layoutCubit
+                                  .pickUserImage(
+                                      imageSource: ImageSource.camera),
+                              pickGalleryImage: () => widget.layoutCubit
+                                  .pickUserImage(
+                                      imageSource: ImageSource.gallery),
+                            );
+                          },
+                          child:
+                              Icon(Icons.edit, color: AppColors.kWhiteColor),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                24.vrSpace,
+                TextFieldWidget(
+                  validator: (val) {
+                    if (val == null || val.isEmpty) {
+                      return "First Name is required";
+                    } else if (val.contains(" ")) {
+                      return "whitespace is not allowed in First Name";
+                    }
+                    return null;
+                  },
+                  textInputAction: TextInputAction.next,
+                  controller: _fnameController,
+                  hint: "First Name",
+                  prefixIconData: Icons.account_circle,
+                ),
+                TextFieldWidget(
+                  validator: (val) {
+                    if (val == null || val.isEmpty) {
+                      return "Last Name is required";
+                    } else if (val.contains(" ")) {
+                      return "whitespace is not allowed in Last Name";
+                    }
+                    return null;
+                  },
+                  textInputAction: TextInputAction.next,
+                  controller: _lnameController,
+                  hint: "Last Name",
+                  prefixIconData: Icons.account_circle,
+                ),
+                TextFieldWidget(
+                  validator: (val) {
+                    if (val!.isEmpty) {
+                      return "Email is required";
+                    }
+                    if (!isValidEmail(val)) {
+                      return "Please enter a valid email address";
+                    }
+                    return null;
+                  },
+                  textInputAction: TextInputAction.next,
+                  controller: _emailController,
+                  hint: "Email",
+                  prefixIconData: Icons.email,
+                ),
+                BlocBuilder<LayoutCubit, LayoutStates>(
+                  buildWhen: (past, current) =>
+                      current is ChangeGenderStatusState,
+                  builder: (context, state) {
+                    return DropDownBtnWidget(
+                      items: const ["male", "female"],
+                      hint: "Choose your gender",
+                      value: widget.layoutCubit.chosenGender,
+                      onChanged: (value) {
+                        setState(() {
+                          widget.layoutCubit.changeGenderStatus(value: value);
+                        });
+                      },
+                    );
+                  },
+                ),
+                8.vrSpace,
+                BlocConsumer<LayoutCubit, LayoutStates>(
+                  listenWhen: (past, current) =>
+                      current is UpdateUserDataWithFailureState ||
+                      current is UpdateUserDataSuccessfullyState ||
+                      current is UpdateUserDataLoadingState,
+                  listener: (context, state) {
+                    if (state is UpdateUserDataWithFailureState) {
+                      showSnackBarWidget(
+                          message: state.message,
+                          successOrNot: false,
+                          context: context);
+                    }
+                    if (state is UpdateUserDataSuccessfullyState) {
+                      showSnackBarWidget(
+                          message: "Your information is updated successfully",
+                          successOrNot: true,
+                          context: context);
+                      Navigator.pop(context);
+                    }
+                  },
+                  builder: (context, state) => state
+                          is UpdateUserDataLoadingState
+                      ? const Align(
+                          alignment: Alignment.center,
+                          child: CircularProgressIndicator(),
+                        )
+                      : BtnWidget(
+                          minWidth: double.infinity,
+                          onTap: () async {
+                            if (!isFormValid(context)) return;
+
+                            if (await checkIfEmailExists(
+                                    _emailController.text) &&
+                                _emailController.text !=
+                                    widget.layoutCubit.user!.email) {
+                              showSnackBarWidget(
+                                  message: "Email already exists",
+                                  successOrNot: false,
+                                  context: context);
+                            } else {
+                              if (formState.currentState!.validate()) {
+                                await updateUserData();
+                              }
+                            }
+                          },
+                          title: "Update",
+                        ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
